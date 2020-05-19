504898385e54cd9f8a740f2189f8a4bb98185645
dani <gehr.danielle@gmail.com>
2020-05-18 21:54:26 -0400

    added jawsbd code

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // set up MySql connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "UnicornLove16",
-    database: "burgers_db"
-});
+var connection;
+
+if(process.env.JAWSBD_URL){
+    connection = mysql.createConnection(process.env.JAWSOB_URL);
+}else{
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "UnicornLove16",
+        database: "burgers_db"
+    });
+};
+
 
 // make connection
 connection.connect(function(err){
